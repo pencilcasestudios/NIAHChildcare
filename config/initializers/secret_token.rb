3b7d6b163eae1430262161bca7dbd38d29df52ea
Silumesii Maboshe <silumesii@pencilcasestudios.com>
2012-12-17 10:51:23 +0200

    Initial project import.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NIAHChildcare::Application.config.secret_token = '29085625be707f78769b22eab8b578aca656c070b9adc7a047ad00012a32afc62b897ec3f741d05130810d22a83bbf3f21a94771799c0ab153cd2d59ec694024'
