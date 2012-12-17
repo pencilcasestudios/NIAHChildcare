b39883d2ebf59f6966b3d7e48a97d2ad0c11c097
Silumesii Maboshe <silumesii@pencilcasestudios.com>
2012-12-17 20:12:02 +0200

    Default layout.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
+  def body_id
+    controller.controller_name
+  end
+
+  def title(page_title)
+    content_for(:title) { page_title }
+  end
+
+  def sub_title(sub_title)
+    content_for(:sub_title) do
+      content_tag :small do
+        sub_title
+      end
+    end
+  end
 end
