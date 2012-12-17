8f8e404c3febec61949e807ceabe762f3792fadd
Silumesii Maboshe <silumesii@pencilcasestudios.com>
2012-12-17 20:59:15 +0200

    Content pages.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,19 @@
 class WelcomeController < ApplicationController
   def index
   end
+
+  def about
+  end
+
+  def fees
+  end
+
+  def nannies
+  end
+
+  def terms
+  end
+
+  def contact
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,18 @@
 NIAHChildcare::Application.routes.draw do
-  get "welcome/index"
+  get "welcome/about"
+  get "welcome/contact"
+  get "welcome/fees"
+  get "welcome/nannies"
+  get "welcome/terms"
+
+
+
+
+  match "about", :to => "welcome#about", :as => "about"
+  match "contact", :to => "welcome#contact", :as => "contact"
+  match "fees", :to => "welcome#fees", :as => "fees"
+  match "nannies", :to => "welcome#nannies", :as => "nannies"
+  match "terms", :to => "welcome#terms", :as => "terms"
 
 
 
