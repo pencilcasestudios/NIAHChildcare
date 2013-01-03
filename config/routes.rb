915ed49a1989cd036767690f20a76906d39d384b
Silumesii Maboshe <silumesii@pencilcasestudios.com>
2013-01-03 10:08:56 +0200

    Routes update.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 NIAHChildcare::Application.routes.draw do
+  get "sessions/new"
+
   get "welcome/about"
   get "welcome/contact"
   get "welcome/fees"
@@ -8,11 +10,19 @@ NIAHChildcare::Application.routes.draw do
 
 
 
-  match "about", :to => "welcome#about", :as => "about"
-  match "contact", :to => "welcome#contact", :as => "contact"
-  match "fees", :to => "welcome#fees", :as => "fees"
-  match "nannies", :to => "welcome#nannies", :as => "nannies"
-  match "terms", :to => "welcome#terms", :as => "terms"
+  match "about", to: "welcome#about", as: "about"
+  match "contact", to: "welcome#contact", as: "contact"
+  match "fees", to: "welcome#fees", as: "fees"
+  match "nannies", to: "welcome#nannies", as: "nannies"
+  match "sign_in", to: "sessions#new", as: "sign_in"
+  match "sign_out", to: "sessions#destroy", as: "sign_out"
+  match "sign_up", to: "users#new", as: "sign_up"
+  match "terms", to: "welcome#terms", as: "terms"
+
+
+
+
+  resources :users
 
 
 
