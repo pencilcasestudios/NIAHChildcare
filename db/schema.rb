e08a21ee2c0827b61c80f875c5def5fd84970c9a
Silumesii Maboshe <silumesii@pencilcasestudios.com>
2013-01-04 17:46:06 +0200

    Database.

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,41 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 0) do
+ActiveRecord::Schema.define(:version => 20130104114053) do
+
+  create_table "simple_captcha_data", :force => true do |t|
+    t.string   "key",        :limit => 40
+    t.string   "value",      :limit => 6
+    t.datetime "created_at",               :null => false
+    t.datetime "updated_at",               :null => false
+  end
+
+  add_index "simple_captcha_data", ["key"], :name => "idx_key"
+
+  create_table "users", :force => true do |t|
+    t.string   "cell_phone_number"
+    t.string   "email"
+    t.string   "first_name"
+    t.string   "language"
+    t.string   "last_name"
+    t.string   "middle_name"
+    t.string   "password_digest"
+    t.string   "role"
+    t.string   "state"
+    t.string   "time_zone"
+    t.datetime "created_at",        :null => false
+    t.datetime "updated_at",        :null => false
+  end
+
+  create_table "versions", :force => true do |t|
+    t.string   "item_type",  :null => false
+    t.integer  "item_id",    :null => false
+    t.string   "event",      :null => false
+    t.string   "whodunnit"
+    t.text     "object"
+    t.datetime "created_at"
+  end
+
+  add_index "versions", ["item_type", "item_id"], :name => "index_versions_on_item_type_and_item_id"
 
 end
