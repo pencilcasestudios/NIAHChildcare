81ac3e83eac6fceffafa67a520866465db9d17ac
Silumesii Maboshe <silumesii@pencilcasestudios.com>
2013-01-21 10:33:45 +0200

    meta-tags installed and configured.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,18 +3,6 @@ module ApplicationHelper
     controller.controller_name
   end
 
-  def title(page_title)
-    content_for(:title) { page_title }
-  end
-
-  def sub_title(sub_title)
-    content_for(:sub_title) do
-      content_tag :small do
-        sub_title
-      end
-    end
-  end
-
   def new_or_create_action?
     (controller.action_name == "new") || (controller.action_name == "create")
   end
@@ -26,5 +14,24 @@ module ApplicationHelper
     elsif alert.to_s == "success"
       level = "success"
     end
+  end
+
+  # Ref: https://github.com/kpumuk/meta-tags
+  def default_meta_tags
+    {
+      description: t("application.meta_tags.description"),
+      keywords: t("application.meta_tags.keywords"),
+      prefix: " ",
+      reverse: true,
+      site: t("application.name"),
+      title: t("application.tagline_html"),
+      open_graph: {
+        description: t("application.meta_tags.description"),
+        image: root_url + asset_path("logo-400x400.png"),
+        title: t("application.tagline_html"),
+        type: :website,
+        url: root_url,
+      }
+    }
   end
 end
