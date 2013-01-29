ec19014fb26d46f1fdf3da29bd6dd5462e5a0f90
Silumesii Maboshe <silumesii@pencilcasestudios.com>
2013-01-29 21:17:38 +0200

    Nanny profile show page.

--- app/controllers/nanny_profiles_controller.rb
+++ app/controllers/nanny_profiles_controller.rb
@@ -5,7 +5,7 @@ class NannyProfilesController < ApplicationController
 
   def new
     @nanny_profile.full_name = current_user.full_name
-    @nanny_profile.addresses.build
+    @nanny_profile.addresses.build(phone: current_user.cell_phone_number, email: current_user.email)
     @nanny_profile.references.build
   end
 
@@ -13,9 +13,21 @@ class NannyProfilesController < ApplicationController
     @nanny_profile.user = current_user
     if @nanny_profile.save
       flash[:success] = t("controllers.nanny_profiles_controller.actions.create.success")
-      redirect_to root_path
+      redirect_to @nanny_profile
     else
       render action: "new"
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @nanny_profile.update_attributes(params[:nanny_profile])
+      flash[:success] = t("controllers.nanny_profiles_controller.actions.update.success")
+      redirect_to @nanny_profile
+    else
+      render action: "edit"
     end
   end
 end
