0d349dcb593200a515eb889f137a942eca62a610
Silumesii Maboshe <silumesii@pencilcasestudios.com>
2013-02-08 11:21:42 +0200

    Making the application form more like Robert's paper form.

--- app/models/family_profile.rb
+++ app/models/family_profile.rb
@@ -7,17 +7,23 @@ class FamilyProfile < ActiveRecord::Base
   has_many :nanny_portraits
   has_many :references, as: :referenceable
 
-  accepts_nested_attributes_for :addresses, reject_if: :all_blank, allow_destroy: true
-  accepts_nested_attributes_for :children, reject_if: :all_blank, allow_destroy: true
-  accepts_nested_attributes_for :guardians, reject_if: :all_blank, allow_destroy: true
-  accepts_nested_attributes_for :references, reject_if: :all_blank, allow_destroy: true
+  validates :statement_of_truth, acceptance: true
+
+  attr_accessor :statement_of_truth
 
   attr_accessible :interests_description
   attr_accessible :pets_description
+  attr_accessible :statement_of_truth
+
 
   # Nested attributes
   attr_accessible :addresses_attributes
   attr_accessible :children_attributes
   attr_accessible :guardians_attributes
   attr_accessible :references_attributes
+
+  accepts_nested_attributes_for :addresses, reject_if: :all_blank, allow_destroy: true
+  accepts_nested_attributes_for :children, reject_if: :all_blank, allow_destroy: true
+  accepts_nested_attributes_for :guardians, reject_if: :all_blank, allow_destroy: true
+  accepts_nested_attributes_for :references, reject_if: :all_blank, allow_destroy: true
 end
