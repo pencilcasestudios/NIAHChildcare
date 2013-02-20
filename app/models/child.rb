b43fce51b80ca68419fa51bc2b99c86f458cb9d1
Silumesii Maboshe <silumesii@pencilcasestudios.com>
2013-02-20 11:39:23 +0200

    Child show fields are now in a partial.

--- app/models/child.rb
+++ app/models/child.rb
@@ -3,6 +3,13 @@ class Child < ActiveRecord::Base
 
   attr_accessible :born_at
   attr_accessible :full_name
+  attr_accessible :gender
   attr_accessible :interests_description
   attr_accessible :special_needs_description
+
+  GENDERS = {
+    # Translation                                 # Database key
+    I18n.t("models.child.genders.male")     =>    "male",
+    I18n.t("models.child.genders.female")    =>    "female",
+  }
 end
