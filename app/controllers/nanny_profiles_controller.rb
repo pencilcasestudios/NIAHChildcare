3bf9523efd5c8faf268f146a4bf85e290c564e87
Silumesii Maboshe <silumesii@pencilcasestudios.com>
2013-02-23 07:45:10 +0200

    Use the original extension when sending the Nanny Profile resume.

--- app/controllers/nanny_profiles_controller.rb
+++ app/controllers/nanny_profiles_controller.rb
@@ -37,9 +37,18 @@ class NannyProfilesController < ApplicationController
   def resume
     nanny_profile = NannyProfile.find(params[:id])
 
+    # Build the resume file name
+    resume = t("controllers.nanny_profiles_controller.actions.resume.copy.resume")
+    owner_name = nanny_profile.user.full_name
+    timestamp = Time.now.strftime("%d %B %Y %H%M")
+    extension = nanny_profile.resume.file.extension
+
+    filename = "#{resume} - #{owner_name} (#{timestamp}).#{extension}"
+
+    # Let the user download it
     send_file(
       nanny_profile.resume_url,
-      filename: "#{t("controllers.nanny_profiles_controller.actions.resume.copy.resume")} - #{nanny_profile.user.full_name} (#{Time.now.strftime("%d %B %Y %H%M")}).pdf",
+      filename: filename,
       x_sendfile: true,
     )
    end
