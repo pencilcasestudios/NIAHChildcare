a842390c442ce45742614b9f5e795f0a69f45552
Silumesii Maboshe <silumesii@pencilcasestudios.com>
2013-04-01 22:47:07 +0200

    Updated seeds to use the tighter validation on the users model.

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,18 +3,22 @@ User.create(
   cell_phone_number: "0000000000",
   email: "silumesii@pencilcasestudios.com",
   first_name: "Silumesii",
+  language: "English",
   last_name: "Maboshe",
   password: "password",
   password_confirmation: "password",
   role: "administrator",
+  time_zone: "Harare",
 )
 
 User.create(
   cell_phone_number: "1111111111",
   email: "Intelcheff@aol.com",
   first_name: "Robert",
+  language: "English",
   last_name: "Jones",
   password: "password",
   password_confirmation: "password",
   role: "administrator",
+  time_zone: "Eastern Time (US & Canada)",
 )
