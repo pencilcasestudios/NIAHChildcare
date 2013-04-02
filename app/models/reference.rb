457697f88dd7210fb47d7988c48263c237ccecec
Silumesii Maboshe <silumesii@pencilcasestudios.com>
2013-04-02 08:52:39 +0200

    Added addresses to Nanny Profile references.

--- app/models/reference.rb
+++ app/models/reference.rb
@@ -1,10 +1,16 @@
 class Reference < ActiveRecord::Base
   belongs_to :referenceable
+
   has_many :addresses, as: :addresseable
 
   attr_accessible :full_name
   attr_accessible :known_since
   attr_accessible :relationship
+
+  # Nested attributes
+  attr_accessible :addresses_attributes
+
+  accepts_nested_attributes_for :addresses, reject_if: :all_blank, allow_destroy: true
 
   validates :full_name, presence: true
   validates :known_since, presence: true
