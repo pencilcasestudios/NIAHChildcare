d71b0c2730ab236fcafb60eef777b2a73d498637
Silumesii Maboshe <silumesii@pencilcasestudios.com>
2013-04-02 08:37:35 +0200

    References for Nanny Profiles.

--- app/models/reference.rb
+++ app/models/reference.rb
@@ -1,7 +1,12 @@
 class Reference < ActiveRecord::Base
   belongs_to :referenceable
+  has_many :addresses, as: :addresseable
 
   attr_accessible :full_name
   attr_accessible :known_since
   attr_accessible :relationship
+
+  validates :full_name, presence: true
+  validates :known_since, presence: true
+  validates :relationship, presence: true
 end
