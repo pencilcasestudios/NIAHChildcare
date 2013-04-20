dd1c5b0c39ed0238976b962fc558992bf8643f00
Silumesii Maboshe <silumesii@pencilcasestudios.com>
2013-04-20 12:40:21 +0200

    Tweaks to registration notification email.

--- app/mailers/emailer.rb
+++ app/mailers/emailer.rb
@@ -3,7 +3,7 @@ class Emailer < ActionMailer::Base
     mail(
         from: "#{t("application.name")} <#{AppConfig.email_user_name}>",
         subject: t("mailers.emailer.registration_notification.subject", application_name: t("application.name"), user_id: user.id),
-        to: "#{user.full_name} <#{user.email}>",
+        to: "#{t("mailers.emailer.registration_notification.to.name")} <#{t("mailers.emailer.registration_notification.to.email")}>",
       )
   end
 end
