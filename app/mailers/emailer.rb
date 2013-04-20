3582bd335901be72bfda4702c53b0269faddfa06
Silumesii Maboshe <silumesii@pencilcasestudios.com>
2013-04-20 14:54:28 +0200

    Updating the administrator group email settings.

--- app/mailers/emailer.rb
+++ app/mailers/emailer.rb
@@ -1,9 +1,9 @@
 class Emailer < ActionMailer::Base
   def registration_notification(user)
     mail(
-        from: "#{t("application.name")} <#{AppConfig.email_user_name}>",
-        subject: t("mailers.emailer.registration_notification.subject", application_name: t("application.name"), user_id: user.id),
-        to: "#{t("mailers.emailer.registration_notification.to.name")} <#{t("mailers.emailer.registration_notification.to.email")}>",
-      )
+      from: "#{t("application.name")} <#{AppConfig.email_user_name}>",
+      subject: t("mailers.emailer.registration_notification.subject", application_name: t("application.name"), user_id: user.id),
+      to: "#{t("mailers.emailer.registration_notification.to.name")} <#{AppConfig.administrator_group_email}>",
+    )
   end
 end
