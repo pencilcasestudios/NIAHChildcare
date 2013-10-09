baa10b03a7596333fe62dccd56099014a7e46da7
Silumesii Maboshe <silumesii@pencilcasestudios.com>
2013-10-09 22:41:43 +0200

    Virtual machine example deployment configuration tweak.

--- config/examples/deploy/virtual.rb
+++ config/examples/deploy/virtual.rb
@@ -1,5 +1,6 @@
 # Ref: http://gist.github.com/293302
 server DEPLOYMENT_CONFIG["virtual_server_name"], :web, :app, :db, primary: true
+
 set :deployment_path, DEPLOYMENT_CONFIG["virtual_deployment_path"]
 set :deploy_to, "/var/Apps/#{application}/#{deployment_path}"
 
