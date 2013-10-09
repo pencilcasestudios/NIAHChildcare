a8092f3dc944e2efb9c303e10375cee7f3ef40b3
Silumesii Maboshe <silumesii@pencilcasestudios.com>
2013-10-09 23:19:52 +0200

    Example staging configuration tweaks.

--- config/examples/deploy/staging.rb
+++ config/examples/deploy/staging.rb
@@ -1,13 +1,14 @@
 # Ref: http://gist.github.com/293302
 server DEPLOYMENT_CONFIG["staging_server_name"], :web, :app, :db, primary: true
-set :deployment_path, DEPLOYMENT_CONFIG["deployment_path_staging"]
+
+set :deployment_path, DEPLOYMENT_CONFIG["staging_deployment_path"]
 set :deploy_to, "/var/Apps/#{application}/#{deployment_path}"
 
 # Don't forget to make this repo on the server
-set :repository_server_name, DEPLOYMENT_CONFIG["repository_server_name"]
+set :repository_server_name, DEPLOYMENT_CONFIG["staging_server_name"]
 set :repository, "#{user}@#{repository_server_name}:/var/Repositories/Git/#{application}.git"
 # Don't forget to make this branch in the repository
-set :branch, DEPLOYMENT_CONFIG["repository_deployment_branch"]
+set :branch, DEPLOYMENT_CONFIG["staging_repository_deployment_branch"]
 
 
 
@@ -21,7 +22,7 @@ namespace :deploy do
   desc "Synchronise assets"
   task :sync_assets do
     # Upload assets to the AssetsVault
-    run_locally "rsync --verbose --stats --progress --compress --archive --partial --recursive --times --perms --links --delete --exclude '.*' --delete-excluded ~/Projects/Rails/NIAHChildcare/app/assets/images/backgrounds/ #{user}@#{server}:#{asset_vault_path}/#{application}/Images/Backgrounds/"
+    run_locally "rsync --verbose --stats --progress --compress --archive --partial --recursive --times --perms --links --delete --exclude '.*' --delete-excluded ~/Projects/Rails/#{application}/app/assets/images/backgrounds/ #{user}@#{DEPLOYMENT_CONFIG["staging_server_name"]}:#{asset_vault_path}/#{application}/Images/Backgrounds/"
 
     # Sync assets from the AssetsVault to the deployed application
     run "rsync --verbose --stats --progress --compress --archive --partial --recursive --times --perms --links --delete --exclude '.*' --delete-excluded #{asset_vault_path}/#{application}/Images/Backgrounds/ #{shared_path}/backgrounds"
