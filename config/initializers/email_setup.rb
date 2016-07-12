8a9404b04e1e6ce671b762733701c2b38bb33165
Silumesii Maboshe <silumesii@pencilcasestudios.com>
2016-07-12 09:55:07 +0200

    Try to send email without TLS.

--- config/initializers/email_setup.rb
+++ config/initializers/email_setup.rb
@@ -2,7 +2,7 @@ ActionMailer::Base.smtp_settings = {
   address:              AppConfig.email_address,
   authentication:       "plain",
   domain:               AppConfig.email_domain,
-  enable_starttls_auto: true,
+  #enable_starttls_auto: true,
   password:             AppConfig.email_password,
   port:                 AppConfig.email_port,
   user_name:            AppConfig.email_user_name,
